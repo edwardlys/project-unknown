bf109db128f5b75ebd4886314bb42cdfd6cafb5e
Edward Lim <elys.1993a@gmail.com>
2021-09-17 19:02:03 +0800

    Added code to test vuln

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -3,12 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $title = 'Home';
+
+        $id = $request->input('id');
+        $pw = $request->input('pw');
+        // if (!empty($id)) {
+        //     $user =  DB::table('users')
+        //         ->select('*')
+        //         ->whereRaw('id = ' . $id)
+        //         ->whereRaw('password = "' . $pw . '"')
+        //         ->first();
+
+        //     if (!empty($user)) {
+        //         echo "I AM LOGGED IN";
+        //     } else {
+        //         echo "WRONG PASSWORD";
+        //     }
+        // }
+
+
+        // if (!empty($id)) {
+        //     $user =  DB::table('users')
+        //         ->select('*')
+        //         ->whereRaw('id = ' . $id)
+        //         ->first();
+
+        //     if (!empty($user)) {
+        //         if ($user->password == $pw) {
+        //             echo "i am logged in";
+        //             die();
+        //         }
+        //     } 
+
+        //     echo "wrong password";
+        //     die();
+        // }
+
 
         return view('home', compact('title'));
     }
